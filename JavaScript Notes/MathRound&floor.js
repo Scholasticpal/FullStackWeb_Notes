61c43b7e3fe47984ea01b5a78aa2fbfeec63ceb8
Anmol Garg <anmolgarg240@gmail.com>
2023-07-08 13:59:34 +0530

    s

--- JavaScript Notes/MathRound&floor.js
+++
@@ -1,8 +0,0 @@
-//Love Calculator
-function valRandom(name1, name2){
-    var n = Math.random();
-    n = n*100;
-    return Math.floor(n)+1; //for 1 to 100
-}
-
-console.log(valRandom("abc", "fd")+"%");
